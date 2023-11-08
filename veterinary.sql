37c1f4fbbf4cfe283dca62225c1809ef9317ab25
kc-12345 <keithcarlomacatangay@gmail.com>
2023-11-08 11:48:27 +0800

    Animal table

--- veterinary.sql
+++ veterinary.sql
@@ -8,3 +8,15 @@ CREATE TABLE owners(
 	email VARCHAR(100) NOT NULL,
 	phone VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE animals(
+	animalid INT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	species VARCHAR(50) NOT NULL,
+	breed VARCHAR(50) NOT NULL,
+	dateofbirth DATE,
+	gender VARCHAR(10) NOT NULL,
+	color VARCHAR(50) NOT NULL,
+	ownerid INT,
+	foreign key (ownerid) references owners(ownerid)
+);
